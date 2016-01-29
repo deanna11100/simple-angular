eebbf3732b400bd96e504a440e8de295d96c238b
Richard Vowles <richard@bluetrainsoftware.com>
2016-01-29 20:33:03 +1300

    Deanna's first application

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,18 @@
-(function() {
+(function(angular) {
   'use strict';
 
   angular.module('sampleApp', [])
     .controller('simpleController', SimpleController);
 
 
-  function SimpleController(scope) {
-    angular.extend(scope, {
-      names: []
+  function SimpleController($scope) {
+    angular.extend($scope, {
+      names: ["Fred","Lexy","Shadow"],
+      myName: "catsarecool"  ,
+      addName: function(){
+        $scope.names.push($scope.myName)  ;
+        $scope.myName = "";
+      }
     });
   };
 
