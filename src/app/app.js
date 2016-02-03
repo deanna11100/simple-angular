13a60fcf508e8d9edb7530eac343248b581103c2
Deanna <deanna11100@gmail.com>
2016-02-03 19:05:15 +1300

    start a new clean website

--- src/app/app.js
+++ src/app/app.js
@@ -6,21 +6,7 @@
 
 
   function SimpleController($scope, $cookies) {
-    angular.extend($scope, {
-      names: [],
-      myName: ""  ,
-      addName: function(){
-        if($scope.myName.startsWith("https://") || $scope.myName.startsWith("http://")) {
-          $scope.names.push($scope.myName)  ;
-          $cookies.putObject("images", $scope.names);
-          $scope.myName = "";
-        }
-      }
-    });
-     var names = $cookies.getObject("images");
-    if(names) {
-      $scope.names = names ;
-    }
+
 
   };
 
